81ae3b877b30547b5fd5d24c436e223ecb5aa539
adlee6 <143027739+adlee6@users.noreply.github.com>
2024-04-14 22:58:46 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,21 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	  if(num/10 > 0) {
+		  digitList.add(num%10);
+		  num = num/10;}
+		else
+		  digitList.add(num);
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		if(digiList.size() == 1)
+			return true;
+		for(int i = 0; i < digitList.size()-1; i++)
+			return false;
+		else
+			return true;
 	}
 	
 	public String toString()
