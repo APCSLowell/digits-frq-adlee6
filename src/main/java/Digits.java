48fee38d49c52381d96e364c1c2586621800e70b
adlee6 <143027739+adlee6@users.noreply.github.com>
2024-04-15 20:51:11 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,12 +8,12 @@ public class Digits
 	public Digits(int num)
 	{ /* to be implemented in part (a) */
 	  digitList = new ArrayList<Integer>();
+	    if(num == 0)
+		digitList.add(0);
 	    while(num > 0) {
 	        digitList.add(0, num%10);
 	        num /= 10;
 	    }
-	    if(num == 0)
-		digitList.add(0);
 	}
 
 	public boolean isStrictlyIncreasing()
