eaf0ef45db5f5b926c878cb0ef8388b1653d42e3
adlee6 <143027739+adlee6@users.noreply.github.com>
2024-04-15 20:43:12 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -19,9 +19,10 @@ public class Digits
 	{ /* to be implemented in part (b) */
 		if(digiList.size() == 1)
 			return true;
-		for(int i = 0; i < digitList.size()-1; i++)
-		  if(digitList.get(i)>=digitList.get(i+1))
-			return false;
+		for(int i = 0; i < digitList.size()-1; i++) {
+		  if(digitList.get(i)>=digitList.get(i+1)){
+			return false; }
+		}
 		return true;
 	}
 	
